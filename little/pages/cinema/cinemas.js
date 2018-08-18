612aed3312bdd687709f6f2afe027bde4091af35
lenovo <2587051793@qq.com>
2018-08-18 21:20:24 +0800

    微信小程序little

---
+++ little/pages/cinema/cinemas.js
@@ -0,0 +1,56 @@
+// 引用百度地图微信小程序JSAPI模块 
+var bmap = require('../../libs/bmap-wx.js');
+var wxMarkerData = [];
+Page({
+  data: {
+    markers: [],
+    latitude: '',
+    longitude: '',
+    rgcData: {}
+  },
+  makertap: function (e) {
+    var that = this;
+    var id = e.markerId;
+    that.showSearchInfo(wxMarkerData, id);
+  },
+  onLoad: function () {
+    var that = this;
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({
+      ak: 'pCTC7lR7KvmTg7BFcEHjrOZ8uy1yOM49'
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      wxMarkerData = data.wxMarkerData;
+      that.setData({
+        markers: wxMarkerData
+      });
+      that.setData({
+        latitude: wxMarkerData[0].latitude
+      });
+      that.setData({
+        longitude: wxMarkerData[0].longitude
+      });
+    }
+    // 发起regeocoding检索请求 
+    BMap.regeocoding({
+      fail: fail,
+      success: success,
+      iconPath: '../../img/marker_red.png',
+      iconTapPath: '../../img/marker_red.png'
+    });
+  },
+  showSearchInfo: function (data, i) {
+    var that = this;
+    that.setData({
+      rgcData: {
+        address: '地址：' + data[i].address + '\n',
+        desc: '描述：' + data[i].desc + '\n',
+        business: '商圈：' + data[i].business
+      }
+    });
+  }
+
+})
---
+++ little/pages/profile/profiles.js
@@ -0,0 +1,28 @@
+var bmap = require('../../libs/bmap-wx.js');
+Page({
+  data: {
+    weatherData: ''
+  },
+  onLoad: function () {
+    var that = this;
+    // 新建百度地图对象 
+    var BMap = new bmap.BMapWX({
+      ak: 'pCTC7lR7KvmTg7BFcEHjrOZ8uy1yOM49'
+    });
+    var fail = function (data) {
+      console.log(data)
+    };
+    var success = function (data) {
+      var weatherData = data.currentWeather[0];
+      weatherData = '城市：' + weatherData.currentCity + '\n' + 'PM2.5：' + weatherData.pm25 + '\n' + '日期：' + weatherData.date + '\n' + '温度：' + weatherData.temperature + '\n' + '天气：' + weatherData.weatherDesc + '\n' + '风力：' + weatherData.wind + '\n';
+      that.setData({
+        weatherData: weatherData
+      });
+    }
+    // 发起weather请求 
+    BMap.weather({
+      fail: fail,
+      success: success
+    });
+  }
+})
