cc11a1d75bd6a7352c7fb174247695a9ec2f2124
lenovo <2587051793@qq.com>
2018-08-29 19:09:14 +0800

    贪吃蛇html+css部分

---
+++ demo/贪吃蛇/index.js
@@ -0,0 +1,7 @@
+var oStart = document.getElementById('start');
+var oPlay = document.getElementById('play');
+var oMain = document.getElementById('main');
+
+oStart.onclick = function(){
+    oPlay.style.display = 'block';
+}
