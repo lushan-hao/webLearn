948ae70aad25d42eb2e66940f14e1cbb170e3503
lenovo <2587051793@qq.com>
2018-09-26 19:32:43 +0800

    vue部分语法

--- reactapp/my-app/src/component/Home/Body.js
+++ reactapp/my-app/src/component/Home/Body.js
@@ -5,9 +5,19 @@ const Item = List.Item;
 const Brief = Item.Brief;
 
 export default class Body extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      dataList:[]
+    }
+  }
+  componentDidMount(){
+    con
+  }
   render() {
     return (
       <div>
+        
         <Item
           arrow="horizontal"
           thumb="https://zos.alipayobjects.com/rmsportal/dNuvNrtqUztHCwM.png"
