90b2214f72d308c1cb12aef0bb322a8082d0e451
haolushan5253 <2587051793@qq.com>
2018-12-08 14:05:06 +0800

    照片

--- vueapp/src/views/book/Book.vue
+++ vueapp/src/views/book/Book.vue
@@ -1,19 +1,37 @@
 <template>
+<div>
     <swipe class="my-swipe">
-        <swipe-item class="slide1"><img src="/img/1.jpg" alt=""></swipe-item>
+        <swipe-item class="slide1"><img :src="bookList.book_cover" alt=""></swipe-item>
         <swipe-item class="slide2"><img src="/img/2.jpg" alt=""></swipe-item>
         <swipe-item class="slide3"><img src="/img/3.jpg" alt=""></swipe-item>
     </swipe>
+    <ul>
+        <li v-for="(book,index) in bookList" :key="index">
+            <img :src="bookList[index].book_cover" alt="">
+        </li>
+    </ul>
+</div>
+    
 </template>
 
 
 <script>
+import axios from 'axios';
 import('vue-swipe/dist/vue-swipe.css');
 import { Swipe, SwipeItem } from 'vue-swipe';
 
 export default {
+    data() {
+        return {
+            bookList: [],
+        };
+    },
     created(){
         this.$emit('switchTab', 'book');
+        axios.get('https://api.myjson.com/bins/dcoqm').then(res=>{
+            this.bookList = res.data.data;
+            console.log(this.bookList);
+        });
     },
     components: {
         swipe: Swipe,
@@ -44,6 +62,10 @@ export default {
 .slide3 {
   background-color: #ff2d4b;
   color: #fff;
+}
+img{
+    width: 1px;
+    float: left;
 }
 </style>
 
